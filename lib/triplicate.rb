79fbe4fa71fb39f87437b8e2ee1e36b5fb93a123
Matt Lyon <matt@flowerpowered.com>
2009-12-29 00:00:31 -0800

    * use [] and []= for getters and setters, not defined methods * revert coercing values in loaded documents until they are accessed

--- lib/triplicate.rb
+++ lib/triplicate.rb
@@ -10,35 +10,68 @@ module Triplicate
   def initialize(doc={})
     @document = doc
   end
-    
+  
+  def [](key)
+    if readable?(key)
+      key = key.to_s
+      @document[key] = Triplicate.coerce(@document[key], coercion(key))
+      @document[key] || default(key)
+    end
+  end
+  
+  def []=(key, value)
+    if writeable?(key)
+      @document[key.to_s] = Triplicate.coerce(value, coercion(key))
+    end
+  end
+  
+  def field(key)
+    self.class.fields[key.to_s]
+  end
+  
+  def readable?(key)
+    field(key)[:read]
+  end
+  
+  def writeable?(key)
+    field(key)[:write]
+  end
+  
+  def default(key)
+    field(key)[:default]
+  end
+  
+  def coercion(key)
+    field(key)[:coerce]
+  end
+  
   module ClassMethods
     
-    def self.extended(base)
+    def field(*keys)
+      opts = keys.last.respond_to?(:has_key?) ? keys.pop : {}
+      [keys].flatten.each do |key|
+        fields[key.to_s] = {
+          :read => true,
+          :write => true,
+          :default => opts[:default],
+          :coerce => opts[:coerce]
+        }
+      end
     end
     
-    def field(key, opts={})
-      define_method key do
-        val = document[key.to_s]
-        if opts[:as]
-          val = Triplicate.coerce(val, opts[:as])
-        end
-        val || opts[:default]
-      end
-      define_method "#{key}=" do |val|
-        if opts[:as]
-          val = Triplicate.coerce(val, opts[:as])
-        end
-        document[key.to_s] = val
-      end
+    def fields
+      @fields ||= Hash.new{|hash, key| hash[key] = {} }
     end
     
   end
-  
+    
   COERCIONS = {}
   COERCIONS[String]         = lambda{|v| v.to_s }
   COERCIONS[Fixnum]         = lambda{|v| v.to_i }
   COERCIONS[Integer]        = lambda{|v| v.to_i }
   COERCIONS[Float]          = lambda{|v| v.to_f }
+  COERCIONS[TrueClass]      = lambda{|v| Triplicate.truthy?(v) }
+  COERCIONS[FalseClass]     = lambda{|v| Triplicate.truthy?(v) }
   COERCIONS[Time]           = lambda{|v| Time.parse(v) }
   COERCIONS[Range]          = lambda do |v|
     raise unless v =~ /^(\d+|\d+\.\d+)(\.{2,3})(\d+|\d+\.\d+)$/
@@ -48,20 +81,36 @@ module Triplicate
     Range.new(start, tail, middle == "...")
   end
   
-  def self.coerce(value, as)
-    as = [as].flatten
-    as.each do |coercion|
-      case coercion
-      when Class
-        next if value.is_a?(coercion)
-        value = if conversion = COERCIONS[coercion]
-          conversion.call(value)
-        else
-          coercion.new(value)
-        end
-      end
+  def self.coerce(value, coercion)
+    if coercion.nil?
+      value
+    elsif value.kind_of?(coercion)
+      value
+    elsif value.is_a?(Hash) && value.has_key?('json_class') && coercion.responds_to?(:json_create)
+      coercion.json_create(value)
+    elsif conversion = COERCIONS[coercion]
+      conversion.call(value)
+    else
+      coercion.new(value)
     end
-    value
+  end
+  
+  def self.truthy?(value)
+    case value
+    when Array
+      arr = value.flatten.compact
+      if arr.size == 1
+        truthy?(arr.first)
+      else
+        !arr.empty?
+      end
+    when String
+      value.strip.gsub(/0+\.?\0*/,'').size > 0
+    when Numeric
+      value != 0
+    else
+      !!value
+    end
   end
   
 end
